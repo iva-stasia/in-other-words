2174785ac80300dc99d9da970a9548e3262dc0d3
Anastasiia Ivanchenko <iva21sta@gmail.com>
2023-08-23 12:55:08 +0300

    fix bugs

--- src/pages/Dictionary.tsx
+++ src/pages/Dictionary.tsx
@@ -1,8 +1,9 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
 import WordTable from "../components/WordTable";
-import { useParams } from "react-router-dom";
+import { useParams, NavLink as RouterLink } from "react-router-dom";
+import { ArrowBackRounded } from "@mui/icons-material";
 
 const Dictionary = () => {
   const { setTitle } = useParams();
@@ -10,7 +11,7 @@ const Dictionary = () => {
   const wordSets = useSelector((state: RootState) => state.words.wordSets);
   let words = useSelector((state: RootState) => state.words.ownWords);
 
-  if (setTitle) {
+  if (setTitle && wordSets.length) {
     const isSetExist = !!wordSets.filter((set) => set.title === setTitle)
       .length;
     if (!isSetExist) throw new Error("Set doesn't exist");
@@ -19,9 +20,37 @@ const Dictionary = () => {
 
   return (
     <Box sx={{ width: "100%", overflowX: "auto", height: "100%" }}>
-      <Typography variant="h5" noWrap component="div" sx={{ fontWeight: 600 }}>
-        {activePage}
-      </Typography>
+      {setTitle ? (
+        <Link
+          component={RouterLink}
+          underline="none"
+          to="/word-sets"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: 1,
+          }}
+        >
+          <ArrowBackRounded />
+          <Typography
+            variant="h5"
+            noWrap
+            component="div"
+            sx={{ fontWeight: 600 }}
+          >
+            {activePage}
+          </Typography>
+        </Link>
+      ) : (
+        <Typography
+          variant="h5"
+          noWrap
+          component="div"
+          sx={{ fontWeight: 600 }}
+        >
+          {activePage}
+        </Typography>
+      )}
       <WordTable words={words} title={setTitle || "All words"} />
     </Box>
   );
